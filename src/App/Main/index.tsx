3e46a3c35fa9d39272d6ec1fa617ba6a209ad2b0
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-31 17:02:27 -0300

    feat: implement Main component and integrate it into App

---
+++ src/App/Main/index.tsx
@@ -0,0 +1,10 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>Main Component</h1>
+      {/* Add your main component content here */}
+    </div>
+  );
+};
+
+export default Main;
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,16 @@
-function App() {
-  return (
-    <>
-      <h1>Sales App</h1>
-      <p>This is the Sales application, part of the micro-frontend architecture.</p>
-      <p>It can be integrated with other applications using Module Federation.</p>
-    </>
-  )
-}
+import { theme } from 'agro-core';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from '@mui/material';
+import { ToastContainer } from 'react-toastify';
+
+import Main from './Main';
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <Main />
+    <ToastContainer />
+  </ThemeProvider>
+);
 
 export default App;
