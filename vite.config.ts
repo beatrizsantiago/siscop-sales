f1c40a560cc1999a3b0b528e3787367bc7857fef
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-31 17:02:20 -0300

    fix: update tsconfig and vite config for improved module resolution and syntax settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { federation } from '@module-federation/vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -16,5 +17,17 @@ export default defineConfig({
   ],
   server: {
     port: 3004,
+  },
+  resolve: {
+    alias: {
+      '@domain': path.resolve(__dirname, 'src/domain'),
+      '@usecases': path.resolve(__dirname, 'src/usecases'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@App': path.resolve(__dirname, 'src/App'),
+      '@fb': path.resolve(__dirname, 'src/firebase'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@phosphor-icons/react'],
   },
 })
